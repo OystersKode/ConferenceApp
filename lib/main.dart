d2038d864010473185c3d556db9ea7d7c30e08a3
SumitDivate63 <sumitdivate3@gmail.com>
2026-03-14 01:17:34 +0530

    firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 import 'routes/app_routes.dart';
 import 'utils/app_theme.dart';
 import 'providers/auth_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MultiProvider(
       providers: [
@@ -26,6 +35,24 @@ class ICSmartConferenceApp extends StatelessWidget {
       initialRoute: AppRoutes.dashboard,
       routes: AppRoutes.routes,
       debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        return Scaffold(
+          body: child,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('test').add({
+                'name': 'Sumit',
+                'project': 'Conference App',
+                'timestamp': FieldValue.serverTimestamp(),
+              });
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Test data added to Firestore!')),
+              );
+            },
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
   }
 }
